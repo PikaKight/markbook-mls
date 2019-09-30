8552c84241287513c2cef0a600fd398fddcc5977
hongs1234 <stella.hong20@ycdsbk12.ca>
2019-09-30 12:51:37 -0400

    pep8 updates

--- menu.py
+++ menu.py
@@ -1,24 +1,24 @@
 from markbook import *
 import json
 
-# Student Options 
+
+# Student Options
 def student():
-   
+
     while True:
-        
-        print ("""
+
+        print("""
         Student OPTIONS
         1: Add
         2: Delete
         3: Edit
         4: Return to menu
         """)
-        
+
         ask = int(input("What you you like to do: "))
-        
-        
+
         if ask == 1:
-            print(add_student_to_classroom(student = input("Enter Student Name (last name, first name):"))) 
+            print(add_student_to_classroom(student = input("Enter Student Name (last name, first name):")))
 
         elif ask == 2:
             print(remove_student_from_classroom(course_code = input("What course is the Student in: "), student = input("Enter Student Name (last name, first name):")))
@@ -29,27 +29,31 @@ def student():
         elif ask == 4:
             break
         else:
-            print ("Not a valid response. Please try again.")
+            print("Not a valid response. Please try again.")
             continue
     menu()
 
-#Course options
+
+# Course options
 def classroom():
-    
+
     while True:
 
-        print ("""
+        print("""
         Course OPTIONS
         1: Create
         2: Delete
         3: Edit
         4: Return to menu
         """)
-        
+
         ask = int(input("What you you like to do: "))
-        
+
         if ask == 1:
-            print(create_classroom(course_code = input("What is the course code:"), course_name = input("What is the course name:"), period = input("What period is the course in:"), teacher = input("Who is the teacher:") ))    
+            print(create_classroom(course_code = input("What is the course code:"),
+            course_name = input("What is the course name:"),
+            period = input("What period is the course in:"),
+            teacher = input("Who is the teacher:")))
         elif ask == 2:
             delete_classroom(course_code = input("Please enter the course code to delete it:"))
         elif ask == 3:
@@ -57,26 +61,27 @@ def classroom():
         elif ask == 4:
             break
         else:
-            print ("Not a valid response. Please try again.")
+            print("Not a valid response. Please try again.")
             continue
 
     menu()
 
-#Assignment Option
+
+# Assignment Option
 def assignment():
 
     while True:
 
-        print ("""
+        print("""
         Assignment OPTIONS
         1: Create
         2: Delete
         3: Edit
         4: Return to menu
         """)
-        
+
         ask = int(input("What you you like to do: "))
-        
+
         if ask == 1:
             print(create_assignment())
         elif ask == 2:
@@ -86,53 +91,54 @@ def assignment():
         elif ask == 4:
             break
         else:
-            print ("Not a valid response. Please try again.")
+            print("Not a valid response. Please try again.")
             continue
 
     menu()
 
-#Markbook View Option
+
+# Markbook View Option
 def markbook():
-  
+
     while True:
 
-        print ("""
+        print("""
         Markbook OPTIONS
         1: View Markbook
         2: Return to menu
         """)
-        
+
         ask = int(input("What you you like to do: "))
-       
+
         if ask == 1:
             with open("classroom.json", "r") as f:
                 markbook = json.load(f)
-            #prints out each course out with line skip
+            # prints out each course out with line skip
             for key, value in markbook.items():
-                print ("""
-                
+                print("""
+
                 {}:{}
-                """.format(key, value))            
+                """.format(key, value))
         elif ask == 2:
             break
         else:
-            print ("Not a valid response. Please try again.")
-            
+            print("Not a valid response. Please try again.")
 
     menu()
 
-#Menu funcition, lets users navigate through the markbook.
+
+# Menu funcition, lets users navigate through the markbook.
 def menu():
 
     print ("""
-        Welcome to Markbook
+        Wlcome to Markbook
         1: Student
         2: Classroom
         3: Assignments
         4: Markbook
         5: Quit
     """)
-    
+
     ask = int(input("What would you like do: "))
 
     if ask == 1:
@@ -149,7 +155,7 @@ def menu():
 
     elif ask == 5:
         q = input("Are you sure you want to quit (y/n):")
-        
+
         if q == "y":
             quit()
         else:
